86ec9ffcee4f68e3ee0fd10570f15cd5f1b9b5be
kenil patel <kenilpatel786@gmail.com>
2020-07-20 21:17:32 -0500

    simple client-server

---
+++ client.py
@@ -0,0 +1,228 @@
+# Name : Kenilkumar Maheshkumar Patel
+# Student Id : 1001765579
+
+# importing important modules for code
+
+import socket
+import pickle
+import re
+import threading
+import time
+from tkinter import *
+import sys
+import os
+from tkinter import font
+''' Started the GUI '''
+root = Tk()
+''' This vaiable stores the name of client and sends it to server'''
+name = StringVar()
+''' This is the main thread that is gonna handle the communication with the
+ server '''
+
+
+class myThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        ''' Socket variable '''
+        self.s = None
+        ''' Port number where client is gonna connect to the server on both of
+         the system port number is same '''
+        self.port = 7398
+        ''' This variable stores the state of the connection it is
+        initialize to 0 that means at the time of creating thread
+        client is not connected to the server '''
+        self.conn = 0
+        ''' This variable stores the information about connection status to
+         the server '''
+        self.status = ""
+        ''' This stores the code which represent different kind of
+         informations '''
+        self.code = 0
+        ''' This variable stores the client name '''
+        self.name_client = ""
+
+    def run(self):
+        while(True):
+            ''' if anytime conn got lost with server then we will try to
+             connect to server again '''
+            if(self.conn == 0):
+                try:
+                    ''' attempting to connect server '''
+                    self.s = socket.socket()
+                    self.s.connect(('127.0.0.1', self.port))
+                    ''' if everythin happens properly then change
+                    conn varible to 1 which represents the
+                    client is successfully connected to client '''
+                    self.conn = 1
+                except Exception as e:
+                    ''' If any exception occurs during attempting the
+                     connection that set the message to
+                     server is not available and conn variable to 0 '''
+                    self.status = "Server is not available"
+                    self.conn = 0
+                    ''' 403 represent server is not available '''
+                    self.code = 403
+            else:
+                try:
+                    ''' receving message from server '''
+                    msg = pickle.loads(self.s.recv(1024))
+                    ''' if message is 404 then server is currently connected
+                     to 3 clients '''
+                    if(msg == 404):
+                        self.status = "server is full please"
+                        self.conn = 0
+                        self.code = 404
+                    elif(re.search("^201:*", str(msg)) != None):
+                        ''' if message is 201 then server is requesting
+                        the name from client '''
+                        x, y = msg.split(":")
+
+                        self.code = 201
+                        self.status = "Server is online"
+                        self.name_client = "client-" + str(y)
+                        ''' sending the name to the server '''
+                        data = pickle.dumps(self.name_client)
+                        self.s.send(data)
+                        self.conn = 1
+                    elif(msg == 200):
+                        ''' server is normally communicating with the client
+                     no special instruction given by server '''
+                        data = pickle.dumps("200")
+                        self.s.send(data)
+                        self.conn = 1
+                        self.status = "Connected"
+                        self.code = 200
+                except Exception as e:
+                    ''' if anything goes wrong from server side then reset
+                    the connection by setting conn to 0 and tries
+                    to attempt the connection again '''
+                    self.name_client = ""
+                    self.status = "Server is not available"
+                    self.conn = 0
+                    self.code = 403
+
+
+''' start the thread to maintain the connection with the server '''
+t = myThread()
+t.start()
+''' function that stops the connection with the server and exit '''
+
+
+def close_window():
+    root.destroy()
+    os._exit(0)
+
+
+''' function to take name of client as input '''
+
+
+def enter_data():
+    ''' take the name ofclient using the textbox '''
+    nts = name.get()
+    if(nts != ""):
+        t.name_client = nts
+    else:
+        t.name_client = ""
+
+
+''' set font size that is going to be used on GUI '''
+myFont = font.Font(size=20)
+myFont1 = font.Font(size=40)
+''' empty label to have some space between the components on GUI '''
+Label(root).pack()
+Label(root).pack()
+''' label to display the warning regarding to name of clients '''
+warning = Label(root)
+warning['font'] = myFont
+warning.pack()
+''' empty label to have some space between the components on GUI '''
+Label(root).pack()
+Label(root).pack()
+''' button to close the client connection to server '''
+btn = Button(root, text="Close connection", command=close_window)
+btn['font'] = myFont
+btn.pack()
+''' entry box to get client name '''
+en = Entry(root, textvariable=name)
+en['font'] = myFont
+''' set the window size to 600x600 '''
+root.geometry("600x800")
+submit = Button(root, text="Connect", command=enter_data)
+submit['font'] = myFont
+''' empty label to have some space between the components on GUI '''
+Label(root).pack()
+Label(root).pack()
+''' dislay the name of client on GUI '''
+cname = Label(root, text="")
+cname['font'] = myFont
+cname.pack()
+''' empty label to have some space between the components on GUI '''
+Label(root).pack()
+''' label to display the reverse counter on GUI '''
+lab = Label(root)
+lab['font'] = myFont1
+''' empty label to have some space between the components on GUI '''
+Label(root).pack()
+Label(root).pack()
+lab.pack()
+Label(root).pack()
+''' empty label to have some space between the components on GUI '''
+Label(root).pack()
+Label(root).pack()
+''' label to display the server status and display some special '''
+status = Label(root)
+status['font'] = myFont
+status.pack()
+''' empty label to have some space between the components on GUI '''
+Label(root).pack()
+Label(root).pack()
+''' call this update function every 50 ms so it will update
+the information on GUI '''
+
+
+def update():
+    if(t.code == 404):
+        ''' if we got code 404 from server then just display
+    the message as Server is full try again after some time '''
+        cname.config(text=t.name_client)
+        status.config(text="Server is full")
+        en.pack_forget()
+        submit.pack_forget()
+    elif(t.code == 201):
+        ''' if we got code 201 from server then just display
+    the message as Server is online '''
+        cname.config(text=t.name_client)
+        status.config(text="Server is online")
+        en.pack()
+        submit.pack()
+    elif(t.code == 200):
+        ''' if we got code 200 from server then everything
+    is normal with server no special instruction '''
+        cname.config(text=t.name_client)
+        warning.config(text="")
+        status.config(text="Server is online")
+        en.pack_forget()
+        submit.pack_forget()
+    elif(t.code == 403):
+        ''' if we got code 403 from server then client j
+    ust lost a connection a with server '''
+        cname.config(text=t.name_client)
+        warning.config(text="")
+        status.config(text="Server is not available at the moment")
+        en.pack_forget()
+        submit.pack_forget()
+    ''' call update function after every 50 ms '''
+    root.after(50, update)
+
+
+update()
+''' close the server when user simply press close button '''
+
+
+def doSomething():
+    os._exit(0)
+
+
+root.protocol('WM_DELETE_WINDOW', doSomething)
+''' start the main GUI window '''
+root.mainloop()
---
+++ server.py
@@ -0,0 +1,214 @@
+# Name : Kenilkumar Maheshkumar Patel
+# Student Id: 1001765579
+
+# importing important modules for code
+import socket
+import pickle
+import threading
+import re
+from random import randint
+import time
+from tkinter import *
+from tkinter import font
+import os
+''' maintain the list of clients id that are connected currently '''
+clients = []
+''' maintain the list of clients name that are connected currently '''
+name = []
+''' initialize the GUI '''
+root = Tk()
+''' both variables are used to represent the message received
+from the client '''
+signal = ""
+signal_disconnected = ""
+count = 1
+''' this thread handle the incoming connection to the client '''
+
+
+class handle_client(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        ''' create a socket '''
+        s = socket.socket()
+        ''' port number to accept the connection from client '''
+        port = 7398
+        s.bind(('', port))
+        s.listen(5)
+        ''' initialize the count to 0 as intially no clients
+        are connected to the server '''
+        count = 0
+        while(True):
+            ''' accept the connection '''
+            c, addr = s.accept()
+            ''' if there are no clients connected then set
+            count to 1 otherwise juts increment the counter '''
+            if(len(clients) == 0):
+                count = 1
+            else:
+                x = sorted(clients)
+                count = int(x[len(clients) - 1]) + 1
+            ''' create the thread to send and recieve the
+            message from client '''
+            t = myThread(c, count)
+            t.start()
+
+
+''' This thread to send and recieve the message from client '''
+
+
+class myThread(threading.Thread):
+    def __init__(self, c, id):
+        threading.Thread.__init__(self)
+        self.c = c
+        self.id = id
+        self.n = ""
+        self.msg = 100
+
+    def run(self):
+        err = 0
+        ''' add the name of client to list of clients connected '''
+        clients.append(self.id)
+        global signal_disconnected
+        ''' if number of connected clients exceeds 3 then just remove
+        the name of client and close the connection '''
+        if(len(clients) == 4):
+            ''' remove the client id from connected clients '''
+            clients.remove(self.id)
+            ''' send the 404 signal to client which means server is full '''
+            self.msg = 404
+            data = pickle.dumps(self.msg)
+            self.c.send(data)
+        else:
+            global count
+            while (True):
+                try:
+                    ''' if server is communicating to client for the
+                    first time then send msg 201 which will ask the
+                    client to send its name to server '''
+                    if(self.msg == 100):
+                        signal_disconnected = ""
+                        self.msg = "201" + ":" +str(count)
+                        count = count + 1
+                    else:
+                        ''' if everything is fine then send msg 200
+                        which represent normal communication with
+                        client without any special instruction '''
+                        self.msg = 200
+                    ''' send the signal to client '''
+                    data = pickle.dumps(self.msg)
+                    self.c.send(data)
+                    ''' recive data from client '''
+                    rdata = pickle.loads(self.c.recv(1024))
+                    if(re.search("^201:*", str(self.msg)) != None):
+                        self.n = rdata
+                        name.append(self.n)
+                        ''' send msg 200 which means normal message
+                        no special instruction '''
+                        self.msg = 200
+                except Exception as e:
+                    ''' if exception occurs then disconnects the
+                    clients '''
+                    print(e)
+                    err = 1
+                    if(self.n != ""):
+                        ''' remove the client from the list of clients
+                        that are connected
+                        construct the signal about which clients
+                        got disconnected and print it on GUI '''
+                        signal_disconnected = self.n + " disconnected"
+                    clients.remove(self.id)
+                    if(self.n != ""):
+                        name.remove(self.n)
+                    break
+
+
+''' function that stops the connection with the server and exit '''
+
+
+def close_window():
+    ''' destroying the windows and exiting the code execution '''
+    root.destroy()
+    os._exit(0)
+
+
+''' setting up the font size '''
+myFont = font.Font(size=20)
+myFont1 = font.Font(size=40)
+''' create empty label to have some space between component '''
+Label(root).pack()
+''' create button to close the server '''
+btn = Button(root, text="Close server", command=close_window)
+btn['font'] = myFont
+btn.pack()
+''' set up the screen size to 600x600 '''
+root.geometry("600x600")
+''' create empty label to have some space between component '''
+Label(root).pack()
+''' create empy label to have some space between component '''
+Label(root).pack()
+''' create empty label to have some space between component '''
+Label(root).pack()
+''' create empty label to have some space between component '''
+Label(root).pack()
+Label(root).pack()
+''' normal label to display message on GUI '''
+head = Label(root, text="Currently connected client")
+head['font'] = myFont
+head.pack()
+''' create empty label to have some space between component '''
+Label(root).pack()
+''' create empty label to have some space between component '''
+Label(root).pack()
+''' label to display name of currently connected clients '''
+cstatus = Label(root)
+cstatus['font'] = myFont
+cstatus.pack()
+''' create empty label to have some space between component '''
+Label(root).pack()
+''' create empty label to have some space between component '''
+Label(root).pack()
+''' label to display name of client who just got disconnected '''
+dstatus = Label(root)
+dstatus['font'] = myFont
+dstatus.pack()
+''' create empty label to have some space between component '''
+Label(root).pack()
+Label(root).pack()
+''' call this update function every 50 ms so it will update the
+information on GUI '''
+
+
+def update():
+    c = ""
+    ''' if there is no client conected then print no one is
+    connected '''
+    if(len(name) == 0):
+        c = "No one is connected"
+    else:
+        ''' if there are some clients that are connected then print it
+        on GUI '''
+        for i in name:
+            c = c + i + "\n"
+    cstatus.config(text=c)
+    ''' signal_disconnected prints the which clients got disconnnected '''
+    dstatus.config(text=signal_disconnected)
+    ''' call update function after every 50 ms '''
+    root.after(50, update)
+
+
+update()
+''' call the thread to handle clients '''
+main_t = handle_client()
+main_t.start()
+''' close the server when user simply press close button '''
+
+
+def doSomething():
+    os._exit(0)
+
+
+root.protocol('WM_DELETE_WINDOW', doSomething)
+''' start the main GUI window '''
+root.mainloop()
